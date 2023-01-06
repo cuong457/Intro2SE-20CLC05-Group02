9ef0415b0069454822dd58cab1a6e80830a6724e
lyvandat <75932518+lyvandat@users.noreply.github.com>
2023-01-06 20:28:11 +0700

    fix merge bugs

--- Server/src/routes/cart.js
+++ Server/src/routes/cart.js
@@ -1,13 +1,9 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
-const {
-  getCart,
-  addItem,
-} = require('../app/controllers/CartController');
+const { getCart } = require("../app/controllers/CartController");
 
-router.patch('/:id/add', addItem);
-router.get('/:id', getCart);
-
+// router.patch('/:id/add', addItem);
+router.get("/:id", getCart);
 
 module.exports = router;
